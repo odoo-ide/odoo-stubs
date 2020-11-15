c7950c5c1e8c25ce4d4a253cb0176cfb5c98e968
Trinh Anh Ngoc <atw1990@gmail.com>
2020-11-15 19:04:07 +0700

    Update stubs

--- odoo-stubs/cli/__init__.pyi
+++ odoo-stubs/cli/__init__.pyi
@@ -1,2 +1,2 @@
-from . import deploy as deploy, populate as populate, scaffold as scaffold, server as server, shell as shell, start as start
+from . import cloc as cloc, deploy as deploy, populate as populate, scaffold as scaffold, server as server, shell as shell, start as start
 from .command import Command as Command, main as main
