17b6e612c48e2cd45bb4f1b89dea8e3a381369e0
Trinh Anh Ngoc <atw1990@gmail.com>
2022-10-05 22:50:27 +0700

    Update stubs

--- odoo-stubs/__init__.pyi
+++ odoo-stubs/__init__.pyi
@@ -1,17 +1,34 @@
-from . import addons as addons, api as api, cli as cli, conf as conf, fields as fields, http as http, loglevels as loglevels, models as models, netsvc as netsvc, osv as osv, release as release, service as service, sql_db as sql_db, tools as tools, upgrade as upgrade
+from psycopg2 import connection
+
+from . import (
+    addons as addons,
+    api as api,
+    cli as cli,
+    conf as conf,
+    fields as fields,
+    http as http,
+    loglevels as loglevels,
+    models as models,
+    netsvc as netsvc,
+    osv as osv,
+    release as release,
+    service as service,
+    sql_db as sql_db,
+    tools as tools,
+    upgrade as upgrade
+)
+from .api import Registry
 from .fields import Command as Command
 from .tools.translate import _ as _, _lt as _lt
-from typing import Any
 
-__path__: Any
 evented: bool
 
-def gevent_wait_callback(conn, timeout: Any | None = ...) -> None: ...
+def gevent_wait_callback(conn: connection, timeout: float | None = ...) -> None: ...
 
 multi_process: bool
 
-def _decompress(data): ...
+def _decompress(data: bytes) -> bytes: ...
 
 SUPERUSER_ID: int
 
-def registry(database_name: Any | None = ...): ...
+def registry(database_name: str | None = ...) -> Registry: ...
--- odoo-stubs/tools/__init__.pyi
+++ odoo-stubs/tools/__init__.pyi
@@ -10,7 +10,20 @@ from .xml_utils import *
 from .date_utils import *
 from .convert import *
 from .template_inheritance import *
-from . import _monkeypatches as _monkeypatches, appdirs as appdirs, cloc as cloc, osutil as osutil, pdf as pdf, pycompat as pycompat, win32 as win32
+from . import (
+    _monkeypatches as _monkeypatches,
+    appdirs as appdirs,
+    cloc as cloc,
+    osutil as osutil,
+    pdf as pdf,
+    pycompat as pycompat,
+    win32 as win32
+)
 from .config import config as config
-from .js_transpiler import ODOO_MODULE_RE as ODOO_MODULE_RE, URL_RE as URL_RE, is_odoo_module as is_odoo_module, transpile_javascript as transpile_javascript
+from .js_transpiler import (
+    ODOO_MODULE_RE as ODOO_MODULE_RE,
+    URL_RE as URL_RE,
+    is_odoo_module as is_odoo_module,
+    transpile_javascript as transpile_javascript
+)
 from .sourcemap_generator import SourceMapGenerator as SourceMapGenerator
