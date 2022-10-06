147af791059b4520c67705c694fbadf4bacb942a
Trinh Anh Ngoc <atw1990@gmail.com>
2022-10-06 10:44:37 +0700

    Update stubs

--- odoo-stubs/tools/mimetypes.pyi
+++ odoo-stubs/tools/mimetypes.pyi
@@ -1,33 +1,32 @@
-from typing import Any, NamedTuple
+import collections
+from re import Pattern
+from typing import Any, Literal
 
-__all__: Any
-_logger: Any
-_ooxml_dirs: Any
+__all__ = ['guess_mimetype']
+_ooxml_dirs: dict[str, str]
 
-def _check_ooxml(data): ...
+def _check_ooxml(data: bytes) -> str | Literal[False]: ...
 
-_mime_validator: Any
+_mime_validator: Pattern
 
-def _check_open_container_format(data): ...
+def _check_open_container_format(data: bytes) -> str | Literal[False]: ...
 
-_xls_pattern: Any
-_ppt_pattern: Any
+_xls_pattern: Pattern
+_ppt_pattern: Pattern
 
-def _check_olecf(data): ...
-def _check_svg(data): ...
+def _check_olecf(data: bytes) -> str | Literal[False]: ...
+def _check_svg(data: bytes) -> str | None: ...
 
-class _Entry(NamedTuple):
-    mimetype: Any
-    signatures: Any
-    discriminants: Any
-_mime_mappings: Any
+_Entry = collections.namedtuple('_Entry', ['mimetype', 'signatures', 'discriminants'])
+_mime_mappings: tuple[_Entry, ...]
 
-def _odoo_guess_mimetype(bin_data, default: str = ...): ...
+def _odoo_guess_mimetype(bin_data: str, default: str = ...) -> str: ...
 
 _guesser: Any
 ms: Any
 
-def guess_mimetype(bin_data, default: Any | None = ...): ...
-guess_mimetype = _odoo_guess_mimetype
+def guess_mimetype(bin_data: str, default: str | None = ...) -> str: ...
 
-def neuter_mimetype(mimetype, user): ...
+def neuter_mimetype(mimetype: str, user: 'odoo.model.res_users') -> str: ...
+
+def get_extension(filename: str) -> str: ...
