aae8214e9ae9c1de24a2ee81aefbc3d109d47920
Trinh Anh Ngoc <atw1990@gmail.com>
2022-10-08 22:42:08 +0700

    Update stubs

--- odoo-stubs/tools/test_reports.pyi
+++ odoo-stubs/tools/test_reports.pyi
@@ -1,8 +1,10 @@
+from typing import Any, Literal
+
 from . import config as config
-from typing import Any
+from ..sql_db import Cursor
 
-_logger: Any
-_test_logger: Any
-
-def try_report(cr, uid, rname, ids, data: Any | None = ..., context: Any | None = ..., our_module: Any | None = ..., report_type: Any | None = ...): ...
-def try_report_action(cr, uid, action_id, active_model: Any | None = ..., active_ids: Any | None = ..., wiz_data: Any | None = ..., wiz_buttons: Any | None = ..., context: Any | None = ..., our_module: Any | None = ...): ...
+def try_report(cr: Cursor, uid: int, rname: str, ids, data: Any | None = ..., context: dict | None = ...,
+               our_module: Any | None = ..., report_type: Any | None = ...) -> bool: ...
+def try_report_action(cr: Cursor, uid: int, action_id: int, active_model: str | None = ..., active_ids: list[int] | None = ...,
+                      wiz_data: dict | None = ..., wiz_buttons: list[str] | None = ..., context: dict | None = ...,
+                      our_module: str | None = ...) -> Literal[True]: ...
