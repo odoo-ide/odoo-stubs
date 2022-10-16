cb2a1c58dc14f3f0000b167e2b80d1a6561700cf
Trinh Anh Ngoc <atw1990@gmail.com>
2022-10-16 16:29:55 +0700

    Update stubs

--- odoo-stubs/cli/__init__.pyi
+++ odoo-stubs/cli/__init__.pyi
@@ -1,2 +1,14 @@
-from . import cloc as cloc, deploy as deploy, populate as populate, scaffold as scaffold, server as server, shell as shell, start as start, tsconfig as tsconfig
+from . import (
+    cloc as cloc,
+    db as db,
+    deploy as deploy,
+    genproxytoken as genproxytoken,
+    neutralize as neutralize,
+    populate as populate,
+    scaffold as scaffold,
+    server as server,
+    shell as shell,
+    start as start,
+    tsconfig as tsconfig
+)
 from .command import Command as Command, main as main
