2310c15d56c2af4094fca6b8627a3d715fa3d671
Trinh Anh Ngoc <atw1990@gmail.com>
2022-10-16 23:39:54 +0700

    Update stubs

--- odoo-stubs/tools/osutil.pyi
+++ odoo-stubs/tools/osutil.pyi
@@ -1,18 +1,10 @@
-import ctypes
 from re import Pattern
-from typing import Any, BinaryIO, Callable, Generator, Iterable, Iterator
+from typing import BinaryIO, Callable, Iterable
 
 WINDOWS_RESERVED: Pattern
 
 def clean_filename(name: str, replacement: str = ...) -> str: ...
 def listdir(dir: str, recursive: bool = ...) -> Iterable[str]: ...
-def walksymlinks(top: str, topdown: bool = ..., onerror: Callable[[OSError], Any] | None = ...) -> Iterator[tuple[str, list[str], list[str]]]: ...
-def tempdir() -> Generator[str, None, None]: ...
 def zip_dir(path: str, stream: str | BinaryIO, include_dir: bool = ..., fnct_sort: Callable | None = ...) -> None: ...
 
-getppid: Callable[[], int]
 is_running_as_nt_service: Callable[[], bool]
-_TH32CS_SNAPPROCESS: int
-
-class _PROCESSENTRY32(ctypes.Structure):
-    _fields_: list[tuple[str, Any]]
