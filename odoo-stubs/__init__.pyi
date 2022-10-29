3bdcf859bb4cf69145ed221f71183f89eb5fe490
Trinh Anh Ngoc <atw1990@gmail.com>
2022-10-29 10:44:04 +0700

    Update stubs

--- odoo-stubs/__init__.pyi
+++ odoo-stubs/__init__.pyi
@@ -1,16 +1,33 @@
-from . import addons as addons, api as api, cli as cli, conf as conf, fields as fields, http as http, loglevels as loglevels, models as models, netsvc as netsvc, osv as osv, release as release, service as service, sql_db as sql_db, tools as tools, upgrade as upgrade
-from .tools.translate import _ as _, _lt as _lt
-from typing import Any, Optional
+from psycopg2 import connection
 
-__path__: Any
+from . import (
+    addons as addons,
+    api as api,
+    cli as cli,
+    conf as conf,
+    fields as fields,
+    http as http,
+    loglevels as loglevels,
+    models as models,
+    netsvc as netsvc,
+    osv as osv,
+    release as release,
+    service as service,
+    sql_db as sql_db,
+    tools as tools,
+    upgrade as upgrade
+)
+from .api import Registry
+from .tools.translate import _ as _, _lt as _lt
+
 evented: bool
 
-def gevent_wait_callback(conn: Any, timeout: Optional[Any] = ...) -> None: ...
+def gevent_wait_callback(conn: connection, timeout: float | None = ...) -> None: ...
 
 multi_process: bool
 
-def _decompress(data: Any): ...
+def _decompress(data: bytes) -> bytes: ...
 
 SUPERUSER_ID: int
 
-def registry(database_name: Optional[Any] = ...): ...
+def registry(database_name: str | None = ...) -> Registry: ...
