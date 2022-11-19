bdede7405843a2ced4f94f3f32cdf08adad37922
Trinh Anh Ngoc <atw1990@gmail.com>
2022-11-19 23:05:09 +0700

    Update stubs

--- odoo-stubs/modules/__init__.pyi
+++ odoo-stubs/modules/__init__.pyi
@@ -1,3 +1,24 @@
-from . import db as db, graph as graph, loading as loading, migration as migration, module as module, registry as registry
-from odoo.modules.loading import load_modules as load_modules, reset_modules_state as reset_modules_state
-from odoo.modules.module import adapt_version as adapt_version, get_module_path as get_module_path, get_module_resource as get_module_resource, get_modules as get_modules, get_modules_with_version as get_modules_with_version, get_resource_from_path as get_resource_from_path, get_resource_path as get_resource_path, initialize_sys_path as initialize_sys_path, load_information_from_description_file as load_information_from_description_file, load_openerp_module as load_openerp_module
+from . import (
+    db as db,
+    graph as graph,
+    loading as loading,
+    migration as migration,
+    module as module,
+    registry as registry
+)
+from .loading import (
+    load_modules as load_modules,
+    reset_modules_state as reset_modules_state
+)
+from .module import (
+    adapt_version as adapt_version,
+    get_module_path as get_module_path,
+    get_module_resource as get_module_resource,
+    get_modules as get_modules,
+    get_modules_with_version as get_modules_with_version,
+    get_resource_from_path as get_resource_from_path,
+    get_resource_path as get_resource_path,
+    initialize_sys_path as initialize_sys_path,
+    load_information_from_description_file as load_information_from_description_file,
+    load_openerp_module as load_openerp_module
+)
