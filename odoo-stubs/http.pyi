f577a35521792e7d26f01b65b67532fade950ef9
Chris Busillo <info@shinycomputers.com>
2025-05-07 10:43:44 -0400

    Update make_response to support additional data types
    Extended the data parameter to accept bytes, bytearray, or None in addition to strings. This ensures greater flexibility and compatibility for response creation.

--- odoo-stubs/http.pyi
+++ odoo-stubs/http.pyi
@@ -261,7 +261,7 @@ class Request:
     def get_json_data(self): ...
     def make_response(
         self,
-        data: str,
+        data: str | bytes | bytearray | None,
         headers: list[tuple[str, Any]] | None = ...,
         cookies: Mapping | None = ...,
         status: int = ...,
